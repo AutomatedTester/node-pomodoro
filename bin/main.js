f515dbee49a3c3b0852c55ac6efe2d22746529b5
AutomatedTester <dburns@mozilla.com>
2013-01-04 00:55:34 +0000

    add the ability to store tasks in a file and remove them

--- bin/main.js
+++ bin/main.js
@@ -9,6 +9,14 @@ var options = nomnom.options({
       help: "This is the task you are going to do. When the pomodoro is" +
         "complete it will say the time is done on that task",
     }
+    , add: {
+      abbr: 'a'
+      , help: "Add a new item that can be processed later"
+    }
+    , remove: {
+      abbr: 'r'
+      , help: "Remove items that we have saved to disk"
+    }
     , version: {
       flag: true
       , abbr: 'v'
@@ -17,7 +25,15 @@ var options = nomnom.options({
   }).parse();
 
 var pomodoro = new Pomodoro();
-if (options._.length != 0) {
+if ((options._.length != 0) && !options.add && !options.remove) {
   pomodoro.run(options._.join(' '));
+}
+
+if (options.add) {
+  pomodoro.add(options._.join(' '));
+}
+
+if (options.remove) {
+  pomodoro.remove(options.remove);
 }
 
